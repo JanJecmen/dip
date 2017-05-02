2a48b6cc3df49f1c08579316bc4d55d8dd899553
Jan Jecmen <jan.jecmen@gmail.com>
2017-05-02 18:45:41 +0200

    plots

---
+++ benchmarks/avg_speedup.r
@@ -0,0 +1,49 @@
+library(ggplot2)
+library(Hmisc)
+library(extrafont)
+loadfonts(device = "win")
+
+# args <- commandArgs(trailingOnly = TRUE)
+args <- "../Desktop/bench/"
+
+files <- sort(list.files(path = args[[1]], pattern = "^benchmark.*\\.csv$"))
+
+if (length(files) < 2) {
+  cat("Not plotting history, less than 2 records found...\n")
+  q()
+}
+
+hashes <- sapply(strsplit(sapply(strsplit(files, "_", fixed = TRUE), tail, n = 1), ".", fixed = TRUE), head, n = 1)
+files <- file.path(args[[1]], files)
+
+combined <- NULL
+
+for (i in seq_along(files)) {
+  d <- read.csv(files[i])
+  gnur <- d[grepl("2 R_ENABLE_JIT=2 vanilla-r", d$experiment),]
+  rir <- d[grepl("3 R_ENABLE_JIT=2 rir", d$experiment),]
+  d <- d[1,]
+  d$experiment <- NULL
+  d$benchmark <- NULL
+  d$time <- NULL
+  d$i <- i
+  gnur <- aggregate(. ~ benchmark, gnur[-1], mean)
+  rir <- aggregate(. ~ benchmark, rir[-1], mean)
+  d$speedup <- mean(gnur$time / rir$time)
+  d$version <- hashes[i]
+  if (is.null(combined)) {
+    combined <- d
+  } else {
+    combined <- rbind.data.frame(combined, d);
+  }
+}
+
+ggplot(combined, aes(x=reorder(version, i), y=speedup, group = 1)) +
+  stat_summary(fun.data = "mean_cl_boot", geom = "smooth") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+  xlab("Revision") +
+  ylab("Speedup [-]") +
+  theme(text = element_text(family="DejaVu Sans"))
+
+ggsave(file.path(args[[1]], "avg_speedup.pdf"), device=cairo_pdf)
+ggsave(file.path(args[[1]], "avg_speedup.png"), scale=1.5)
--- benchmarks/overall.r
+++ benchmarks/overall.r
@@ -27,21 +27,30 @@ before <- before[grepl("3 R_ENABLE_JIT=2 rir", before$experiment),]
 before$experiment <- NULL
 before <- aggregate(. ~ benchmark, data=before, mean)
 before$time <- before$time / gnur$time
+tmp <- mean(before$time)
 before$Status <- "before"
+before$benchmark <- factor(before$benchmark, levels=c(levels(before$benchmark), "AVERAGE"))
+before <- rbind(before, list("AVERAGE", tmp, "mean before"))
 before$benchmark <- as.ordered(before$benchmark)
 
 after <- after[grepl("3 R_ENABLE_JIT=2 rir", after$experiment),]
 after$experiment <- NULL
 after <- aggregate(. ~ benchmark, data=after, mean)
 after$time <- after$time / gnur$time
+tmp <- mean(after$time)
 after$Status <- "final"
+after$benchmark <- factor(after$benchmark, levels=c(levels(after$benchmark), "AVERAGE"))
+after <- rbind(after, list("AVERAGE", 0, "mean final"))
 after$benchmark <- as.ordered(after$benchmark)
 
-ordering <- before$benchmark[order(reorder(as.ordered(before$benchmark), -before$time))]
+ordering <- after$benchmark[order(reorder(after$benchmark, -after$time))]
+after[nrow(after),"time"] <- tmp
 
 d <- rbind(before, after)
 
-gnur$time <- gnur$time / gnur$time
+gnur$benchmark <- factor(gnur$benchmark, levels=c(levels(gnur$benchmark), "AVERAGE"))
+gnur <- rbind(gnur, list("AVERAGE", 1))
+gnur$time <- 1
 
 plot <- ggplot(d)
 
@@ -57,7 +66,7 @@ plot +
   scale_fill_brewer(palette="Paired") +
   scale_x_discrete(limits = ordering) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.33, hjust = 1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   xlab("Benchmark") +
   ylab("Speedup vs. GNU R") +
   theme(text = element_text(family="DejaVu Sans"))
